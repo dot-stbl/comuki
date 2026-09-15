1a76069a4315926aa17d2ffde2022c68b220564e
lowern1ght <lowern1ght@users.noreply.github.com>
2026-09-15 17:06:47 +0500

    [.stbl](fix/compute): add using for ILoggerFactory

--- platform/src/engine/Comuki.Engine.Compute/Installers/ComputeInstaller.cs
+++ platform/src/engine/Comuki.Engine.Compute/Installers/ComputeInstaller.cs
@@ -13,6 +13,7 @@ using k8s;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace Comuki.Engine.Compute.Installers;
